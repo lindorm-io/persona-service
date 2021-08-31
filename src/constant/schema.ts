371e340831e33ddb1599d275c9f6498f1a901481
Jonn Nilsson <jonn@jonn.nu>
2021-08-31 16:44:35 +0200

    fix: update constants

--- src/constant/schema.ts
+++ src/constant/schema.ts
@@ -1,11 +1,30 @@
 import Joi from "joi";
+import { IdentifierType, NamingSystem } from "../enum";
+
+export const JOI_BIRTHDATE = Joi.string().regex(/^[0-9]{4}-[0-9]{2}-[0-9]{2}$/);
+
+export const JOI_EMAIL = Joi.string()
+  .case("lower")
+  .email({
+    minDomainSegments: 2,
+    tlds: { deny: [] },
+  });
+
+export const JOI_GUID = Joi.string().guid({ version: "uuidv4" });
+
+export const JOI_IDENTIFIER_TYPE = Joi.string().valid(
+  IdentifierType.EMAIL,
+  IdentifierType.PHONE_NUMBER,
+  IdentifierType.OIDC,
+  IdentifierType.USERNAME,
+);
 
 export const JOI_IDENTITY_ADDRESS = Joi.object({
   country: Joi.string().allow(null).required(),
   locality: Joi.string().allow(null).required(),
   postalCode: Joi.string().allow(null).required(),
   region: Joi.string().allow(null).required(),
-  streetAddress: Joi.string().allow(null).required(),
+  streetAddress: Joi.array().items(Joi.string()).required(),
 });
 
 export const JOI_IDENTITY_DISPLAY_NAME = Joi.object({
@@ -13,12 +32,25 @@ export const JOI_IDENTITY_DISPLAY_NAME = Joi.object({
   number: Joi.number().allow(null).required(),
 });
 
-export const JOI_DATE = Joi.string().regex(/^[0-9]{4}-[0-9]{2}-[0-9]{2}$/);
-
-export const JOI_GUID = Joi.string().guid({ version: "uuidv4" });
-
 export const JOI_LOCALE = Joi.string().regex(/^[a-z]{2}-[A-Z]{2}$/);
 
-export const JOI_PHONE = Joi.string().regex(/^\+?[1-9]\d{1,14}$/);
+export const JOI_NAMING_SYSTEM = Joi.string().valid(
+  NamingSystem.GIVEN_FAMILY,
+  NamingSystem.FAMILY_GIVEN,
+);
 
-export const JOI_ZONE_INFO = Joi.string().regex(/^([A-Z][a-z]+)\/([A-Z][a-z]+)(_[A-Z][a-z]+)*$/);
+export const JOI_OPENID_ADDRESS = Joi.object({
+  country: Joi.string().allow(null).required(),
+  locality: Joi.string().allow(null).required(),
+  postalCode: Joi.string().allow(null).required(),
+  region: Joi.string().allow(null).required(),
+  streetAddress: Joi.string().allow(null).required(),
+});
+
+export const JOI_PHONE_NUMBER = Joi.string().regex(/^\+?[1-9]\d{1,14}$/);
+
+export const JOI_SIGNATURE = Joi.string().base64();
+
+export const JOI_ZONE_INFO = Joi.string().regex(
+  /^([A-Z][a-z]+)\/([A-Z][a-z]+)(_[A-Z][a-z]+)*$/,
+);
