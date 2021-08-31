5c6e9cf96dc2104c2fcde460da7ffc05f1358c68
Jonn Nilsson <jonn@jonn.nu>
2021-08-31 16:42:14 +0200

    fix: update logger

--- src/logger/winston.ts
+++ src/logger/winston.ts
@@ -1,17 +1,18 @@
+import { NODE_ENVIRONMENT } from "../config";
 import { Logger, LogLevel } from "@lindorm-io/winston";
-import { IS_TEST, NODE_ENVIRONMENT } from "../config";
 import { NodeEnvironment } from "@lindorm-io/koa-config";
 import { join } from "path";
 import { readFileSync } from "fs";
-import { sanitiseToken } from "@lindorm-io/jwt";
 
-const pkg = readFileSync(join(__dirname, "..", "..", "package.json"), { encoding: "utf8" });
+const pkg = readFileSync(join(__dirname, "..", "..", "package.json"), {
+  encoding: "utf8",
+});
 const { name, version } = JSON.parse(pkg);
 
 export const winston = new Logger({
   packageName: name,
   packageVersion: version,
-  test: IS_TEST,
+  test: NODE_ENVIRONMENT === NodeEnvironment.TEST,
 });
 
 if (NODE_ENVIRONMENT === NodeEnvironment.PRODUCTION) {
@@ -21,8 +22,6 @@ if (NODE_ENVIRONMENT === NodeEnvironment.PRODUCTION) {
   winston.addFileTransport(LogLevel.VERBOSE);
   winston.addFileTransport(LogLevel.DEBUG);
   winston.addFileTransport(LogLevel.SILLY);
-
-  winston.addFilter("request.header.authorization", sanitiseToken);
 }
 
 if (NODE_ENVIRONMENT !== NodeEnvironment.PRODUCTION) {
